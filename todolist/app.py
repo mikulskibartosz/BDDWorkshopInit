756c9551225dd959310aed3944b87d8f56137d25
Bartosz Mikulski <mail@mikulskibartosz.name>
2021-07-06 17:00:44 +0200

    zmiana na pipenv

---
+++ todolist/app.py
@@ -0,0 +1,28 @@
+from flask import Flask, jsonify, request
+
+
+app = Flask(__name__)
+
+tasks = []
+
+@app.route('/tasks')
+def get_tasks():
+    global tasks
+    return jsonify(tasks)
+
+@app.route('/tasks', methods=['POST'])
+def create_task():
+    global tasks
+    task = request.get_json()
+    task_id = len(tasks)
+    task['id'] = task_id
+    tasks.append(task)
+    return '', 201
+
+@app.route('/tasks/completed/<int:id>', methods=['PUT'])
+def complete_task(id):
+    global tasks
+    to_remove =  list(filter(lambda x: x['id'] == id, tasks))
+    for removed in to_remove:
+        tasks.remove(removed)
+    return '', 202
